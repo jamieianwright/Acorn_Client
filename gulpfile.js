d7e5a8bf00c561ceadcb77c5dde133baf5fbbefe
cjlovescoffee <cjlovescoffee@gmail.com>
2018-10-20 17:57:56 +0100

    Add gulp, sass

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+    return gulp.src("src/scss/styles.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('public/assets/css'));
+});
+
+gulp.task('default', ['sass']);
