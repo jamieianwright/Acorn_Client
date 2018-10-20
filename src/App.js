cd1b3f718647915eea30417e8249f411775b8507
cjlovescoffee <cjlovescoffee@gmail.com>
2018-10-20 18:35:24 +0100

    Add Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>Suppliers motherfucker</div>
+      <div className="container">This is a container</div>
     );
   }
 }
