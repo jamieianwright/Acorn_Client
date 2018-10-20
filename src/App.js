2e5b2a34f8ce8dcfd3990e865d190d48a1e8c41e
cjlovescoffee <cjlovescoffee@gmail.com>
2018-10-20 19:25:28 +0100

    Add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  } from 'reactstrap';
+
 import Suppliers from './Suppliers';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div>
-      <header>
-        <div className="container">
-          <div className="row">
-            <div className="col-auto">
-              <ul className="list-inline">
-                <li className="list-inline-item"><Link to="/">Dashboard</Link></li>
-                <li className="list-inline-item"><Link to="/suppliers">Suppliers</Link></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
+        <Navbar color="light" light expand="md">
+          <Container>
+            <NavbarBrand href="/">Dashboard</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="/suppliers">Suppliers</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -31,11 +56,11 @@ class App extends Component {
 }
 
 const PageNotFound = () => (
-  <div>Page not found!</div>
+  <Container>Page not found!</Container>
 )
 
 const Home = () => (
-  <div>Dashboard coming soon!</div>
+  <Container>Dashboard coming soon!</Container>
 )
 
 export default App;
