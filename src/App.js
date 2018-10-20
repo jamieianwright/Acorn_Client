c41ea2416af152692cd8e8161708a1e8c31ec5ae
Dan <smellsmell@hotmail.co.uk>
2018-10-20 17:08:19 +0100

    add text to test heroku deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>Suppliers</div>
+      <div>Suppliers motherfucker</div>
     );
   }
 }
