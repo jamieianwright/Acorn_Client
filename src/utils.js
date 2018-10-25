073ea2f76fda75bd02e4c090abc7c26c5615cc56
cjlovescoffee <cjlovescoffee@gmail.com>
2018-10-25 15:11:24 +0100

    Add utils, string truncate function

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+function truncateString(input, char_limit = 32, suffix = '...') {
+  if (input.length <= char_limit) {
+      return input;
+    }
+    else {
+      return input.substring(0, char_limit).trim() + suffix;
+    }
+}
