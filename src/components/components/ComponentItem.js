4e05c116e8472d2543249c008b1525c286b09099
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-01-12 18:28:22 +0000

    Supplier displayed in components table

--- src/components/components/ComponentItem.js
+++ src/components/components/ComponentItem.js
@@ -3,6 +3,7 @@ import { truncateString } from '../../utils.js';
 
 export class ComponentItem extends Component {
   render() {
+    console.log(this.props)
     return (
         <tr>
           <th>{truncateString(this.props.name)}</th>
@@ -10,7 +11,7 @@ export class ComponentItem extends Component {
           <th>{truncateString(this.props.description)}</th>
           <th>{truncateString(this.props.lead_time, 2)} days</th>
           <th>{truncateString(this.props.min_order_quantity, 4)}</th>
-          <th></th>
+          <th>{truncateString(this.props.supplier.name, 15)}</th>
           <th></th>
         </tr>
     )
